daf432cc2ed56a8d31f775c50a05485e47747b1c
TimDenker0 <64010582+TimDenker0@users.noreply.github.com>
2022-07-19 11:15:42 +0200

    Update SHAB_-_09_-_Time.ino

--- SHAB - Code/SHAB_-_09_-_Time.ino
+++ SHAB - Code/SHAB_-_09_-_Time.ino
@@ -93,8 +93,8 @@ Serial.begin(9600);
 
 
 //----- WLAN -----
-    wifiMulti.addAP("666", "62729030318813756005");
-    wifiMulti.addAP("Pixel_1308", "12345678");
+    wifiMulti.addAP("666", "Passwort1");
+    wifiMulti.addAP("Pixel_1308", "Passwort2");
     wifiMulti.addAP("ssid_from_AP_3", "your_password_for_AP_3");
 
     Serial.println("Connecting Wifi...");
